e94aae332b032e8c46519f60a6079fd1853f8708
Ruslan Ishmametov <russ-i@yandex.ru>
2018-03-24 17:53:43 +0300

    1.Верстка первого и второго слайда

--- index.php
+++ index.php
@@ -5,17 +5,71 @@
 	<meta name="viewport"
 	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
 	<link rel="stylesheet" href="style.css">
 	<title>Document</title>
 </head>
 <body>
-	<div class="container main" style="background-image: url('maket/02_PREVIEWS/01_One Page.jpg')">
-		<div class="row">
-			<div class="header">
-
-			</div>
-		</div>
-
+<main>
+<section class="first-slide">
+	<div class="logo">
+		<img src="image/logo.png" alt="logo">
 	</div>
+	<nav class="menu">
+		<ul>
+			<li><a href="#">HOME</a></li>
+			<li><a href="#">ABOUT</a></li>
+			<li><a href="#">WORK</a></li>
+			<li><a href="#">PROCESS</a></li>
+			<li><a href="#">SERVICES</a></li>
+			<li><a href="#">TESTIMONIALS</a></li>
+			<li><a href="#">CONTACT</a></li>
+		</ul>
+	</nav>
+	<article class="info">
+		<header class="info__caption">
+			<p>We Design and Develop</p>
+		</header>
+		<div class="info__text">
+			<p>We are a new design studio based in USA. We have over
+				20 years of combined experience, and know a thing or two
+				about designing websites and mobile apps.</p>
+		</div>
+		<button>CONTACT US</button>
+	</article>
+</section>
+
+<section class="second-slide">
+	<article class="about-us">
+		<header>
+			<p class="about-us-caption">About Us</p>
+		</header>
+		<p class="about-us-text">Divide have don't man wherein air fourth. Own itself make have night won't make.
+			A you under Seed appear which good give. Own give air without fowl moveth dry first
+			heaven fruit, dominion she'd won't very all.
+		</p>
+	</article>
+	<div class="signature">
+		<img src="image/signature.png" alt="">
+	</div>
+</section>
+<section class="third-slide">
+	<div class="third-slide-left">
+		<header class="skills">
+			<p>Professional Skills</p>
+		</header>
+	</div>
+	<div class="third-slide-right"></div>
+</section>
+<section></section>
+<section></section>
+<section></section>
+<section></section>
+<section></section>
+<section></section>
+<section></section>
+<section></section>
+</main>
+
 </body>
 </html>
