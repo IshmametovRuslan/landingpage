bb3db4b976300502e315aec3618d7b33b29a48b4
Ruslan Ishmametov <russ-i@yandex.ru>
2018-03-31 21:47:42 +0300

    1.Верстка третьего и четвертого слайда

--- index.php
+++ index.php
@@ -7,68 +7,133 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 
 	<link rel="stylesheet" href="style.css">
-	<title>Document</title>
+	<title>Landing page</title>
 </head>
 <body>
 <main>
-<section class="first-slide">
-	<div class="logo">
-		<img src="image/logo.png" alt="logo">
-	</div>
-	<nav class="menu">
-		<ul>
-			<li><a href="#">HOME</a></li>
-			<li><a href="#">ABOUT</a></li>
-			<li><a href="#">WORK</a></li>
-			<li><a href="#">PROCESS</a></li>
-			<li><a href="#">SERVICES</a></li>
-			<li><a href="#">TESTIMONIALS</a></li>
-			<li><a href="#">CONTACT</a></li>
-		</ul>
-	</nav>
-	<article class="info">
-		<header class="info__caption">
-			<p>We Design and Develop</p>
-		</header>
-		<div class="info__text">
-			<p>We are a new design studio based in USA. We have over
-				20 years of combined experience, and know a thing or two
-				about designing websites and mobile apps.</p>
+	<section class="first-slide">
+		<div class="logo">
+			<img src="image/logo.png" alt="logo">
 		</div>
-		<button>CONTACT US</button>
-	</article>
-</section>
+		<nav class="menu">
+			<ul>
+				<li><a href="#">HOME</a></li>
+				<li><a href="#">ABOUT</a></li>
+				<li><a href="#">WORK</a></li>
+				<li><a href="#">PROCESS</a></li>
+				<li><a href="#">SERVICES</a></li>
+				<li><a href="#">TESTIMONIALS</a></li>
+				<li><a href="#">CONTACT</a></li>
+			</ul>
+		</nav>
+		<article class="info">
+			<header class="info__caption">
+				<p>We Design and Develop</p>
+			</header>
+			<div class="info__text">
+				<p>We are a new design studio based in USA. We have over
+					20 years of combined experience, and know a thing or two
+					about designing websites and mobile apps.</p>
+			</div>
+			<button>CONTACT US</button>
+		</article>
+	</section>
 
-<section class="second-slide">
-	<article class="about-us">
-		<header>
-			<p class="about-us-caption">About Us</p>
-		</header>
-		<p class="about-us-text">Divide have don't man wherein air fourth. Own itself make have night won't make.
-			A you under Seed appear which good give. Own give air without fowl moveth dry first
-			heaven fruit, dominion she'd won't very all.
-		</p>
-	</article>
-	<div class="signature">
-		<img src="image/signature.png" alt="">
-	</div>
-</section>
-<section class="third-slide">
-	<div class="third-slide-left">
-		<header class="skills">
-			<p>Professional Skills</p>
-		</header>
-	</div>
-	<div class="third-slide-right"></div>
-</section>
-<section></section>
-<section></section>
-<section></section>
-<section></section>
-<section></section>
-<section></section>
-<section></section>
-<section></section>
+	<section class="second-slide">
+		<article class="about-us">
+			<header>
+				<p class="about-us-caption">About Us</p>
+			</header>
+			<p class="about-us-text">Divide have don't man wherein air fourth. Own itself make have night won't make.
+				A you under Seed appear which good give. Own give air without fowl moveth dry first
+				heaven fruit, dominion she'd won't very all.
+			</p>
+		</article>
+		<div class="signature">
+			<img src="image/signature.png" alt="">
+		</div>
+	</section>
+	<section class="third-slide">
+		<div class="third-slide-left">
+			<header class="skills">
+				<p id="header">Professional Skills</p>
+			</header>
+			<div class="skills-cale">
+				<div class="skills-group">
+					<p>UI/UX DESIGN 75%</p>
+					<div class="skills-design-scale">
+						<progress max="100" value="75">
+					</div>
+				</div>
+				<div class="skills-group">
+					<p>WEB DEVELOPMENT 90%</p>
+					<div class="skills-development-scale">
+						<progress max="100" value="90">
+					</div>
+				</div>
+				<div class="skills-group">
+					<p>MARKETING 65%</p>
+					<div class="skills-marketing-scale">
+						<progress max="100" value="65">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="third-slide-right"></div>
+	</section>
+	<section class="four-slide">
+		<article>
+			<div class="portfolio">
+				<div class="portfolio-image">
+					<img src="image/icon/pe-7s-portfolio.png" alt="portfolio">
+				</div>
+				<div class="portfolio-text">
+					<p>548</p>
+					<p>PROJECT COMPLETED</p>
+				</div>
+			</div>
+		</article>
+		<article>
+			<div class="clock">
+				<div class="clock-image">
+					<img src="image/icon/pe-7s-clock.png" alt="clock">
+				</div>
+				<div class="clock-text">
+					<p>1465</p>
+					<p>WORKING HOURS</p>
+				</div>
+			</div>
+		</article>
+		<article>
+			<div class="star">
+				<div class="star-image">
+					<img src="image/icon/pe-7s-star.png" alt="star">
+				</div>
+				<div class="star-text">
+					<p>612</p>
+					<p>POSITIVE FEEDBACKS</p>
+				</div>
+			</div>
+		</article>
+		<article>
+			<div class="likes">
+				<div class="likes-image">
+					<img src="image/icon/pe-7s-like.png" alt="like">
+				</div>
+				<div class="likes-text">
+					<p>735</p>
+					<p>HAPPY CLIENTS</p>
+				</div>
+			</div>
+		</article>
+	</section>
+	<section></section>
+	<section></section>
+	<section></section>
+	<section></section>
+	<section></section>
+	<section></section>
+	<section></section>
 </main>
 
 </body>
